192870a0974557a1c46176be28a061a2931f66b4
Fatima Blanco <fatimablancomunoz@gmail.com>
2022-04-25 14:23:26 +0200

    feat: add animate to loading and remove setTimeout

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -9,13 +9,12 @@ export const useFetchGifs = (category) => {
 
     useEffect(() => {
         getGifs(category).then(imgs => {
-            setTimeout(() => {
-                setState({
-                    data: imgs,
-                    loading: false,
-                })
-            }, 3000)
-          
+
+            setState({
+                data: imgs,
+                loading: false,
+            })
+
         })
 
     }, [category])
