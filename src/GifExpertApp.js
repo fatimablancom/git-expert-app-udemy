dcd8ae4a12992537481c5e9fdc7fe8cf539caa70
Fatima Blanco <fatimablancomunoz@gmail.com>
2022-04-25 13:21:51 +0200

    feat: new component - GifExpertApp - useState and send props to other component

---
+++ src/GifExpertApp.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+// import PropTypes from 'prop-types'
+import { AddCategory } from './components/AddCategory';
+import GifGrid from './components/GifGrid';
+
+const GifExpertApp = props => {
+
+    const [categories, setCategories] = useState([]);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories}/>
+            <hr />
+
+            <ol>
+                {categories.map(category => 
+                   <GifGrid 
+                        category={category}
+                        key={category}
+                    />
+                )}
+            </ol>
+            
+        </>
+    )
+}
+
+// GifExpertApp.propTypes = {}
+
+export default GifExpertApp
